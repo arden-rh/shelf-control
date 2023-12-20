d180bf61b34a7a4e1383895bd506a9ae3963868f
Arden <arden.haldorson@gmail.com>
2023-12-20 13:49:24 +0100

    setup of Firebase Auth with login page and register page, setup of emulators to run tests locally

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { session} from '$lib/session';
+    import type { SessionState, User } from '$lib/session';
+	import { goto } from '$app/navigation';
+	import { signOut } from 'firebase/auth';
+	import { auth } from '$lib/firebase.client';
+
+	import type { LayoutData } from './$types';
+	export let data: LayoutData;
+
+	let loading: boolean = true;
+	let loggedIn: boolean = false;
+
+	session.subscribe((current: SessionState) => {
+		loading = current?.loading;
+		loggedIn = current?.loggedIn;
+	});
+
+	onMount(async () => {
+		const user: any = await data.getAuthUser();
+
+		const loggedIn = !!user;
+        
+		session.update((current: SessionState) => {
+			loading = false;
+			return {
+				...current,
+				user,
+				loggedIn,
+				loading: false
+			};
+		});
+
+		if (loggedIn) {
+			goto('/');
+		}
+	});
+</script>
+
+{#if loading}
+	<div>Loading...</div>
+{:else}
+	<div>
+		Logged in: {loggedIn}
+		<slot />
+	</div>
+{/if}
